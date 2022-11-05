fa0cf7bb9b45388a68294dfa9c1429621b007dd3
Harjaap Dhillon <harjaapdhillon@ip-192-168-1-3.ec2.internal>
2022-11-05 22:44:43 +0530

    changes

--- src/utils/api/apiUrl.ts
+++ src/utils/api/apiUrl.ts
@@ -1,6 +1,6 @@
 const urls = {
   hostedURL: "https://us-central1-trendly-40221.cloudfunctions.net/api",
-  localURL: "http://192.168.1.4:80",
+  localURL: "http://192.168.1.3:80",
 };
 
-export const url = urls.localURL;
+export const url = urls.hostedURL;
