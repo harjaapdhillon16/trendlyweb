e890ff8f73e63f0cdceba0e62d1de2a98eaf7156
Harjaap Dhillon <harjaapdhillon@Harjaaps-MacBook-Pro.local>
2022-11-13 23:43:56 +0530

    main

--- src/utils/api/apiUrl.ts
+++ src/utils/api/apiUrl.ts
@@ -1,6 +1,6 @@
 const urls = {
-  hostedURL: "https://us-central1-trendly-40221.cloudfunctions.net/api",
+  hostedURL: "https://api-eqigbdfnyq-uc.a.run.app",
   localURL: "http://192.168.1.3:80",
 };
 
-export const url = urls.localURL;
+export const url = urls.hostedURL;
